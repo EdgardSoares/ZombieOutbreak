5bff02df249a6d9e26e6d4231c99fe00650d44a6
Edgard Thobias <edgardsoares_ed@hotmail.com>
2020-09-23 00:12:39 -0400

    Project Update
    Modificando no script Player.cs  - Adicionando velocidade para o personagem

---
+++ ZombieOut/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float _eixoX;
+    public float _eixoZ;
+    Vector3 _direction = Vector3.zero;
+    public float _speed;
+    // Update is called once per frame
+    void Update()
+    {
+        _eixoX = Input.GetAxis("Horizontal");
+        _eixoZ = Input.GetAxis("Vertical");
+
+        _direction = new Vector3(_eixoX, 0, _eixoZ);
+        transform.Translate(_direction * _speed * Time.deltaTime);
+    }
+}
