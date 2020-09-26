08c2e830f2a87149dd7d44bb01f887da845ecf1d
Edgard Thobias <edgardsoares_ed@hotmail.com>
2020-09-25 23:46:17 -0400

    Projetc Update
    - Adicionado o Script CameraController: Para controlar a camera seguindo o player.

---
+++ ZombieOut/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject _player;
+    Vector3 _distanciaCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _distanciaCam = transform.position - _player.transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _player.transform.position + _distanciaCam;
+    }
+}
